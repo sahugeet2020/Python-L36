a3f1fb0898b707598fdce358b2a1eac970e88403
sahugeet2020 <sahugeet2020@gmail.com>
2025-08-01 18:55:17 +0530

    Update txt.py

--- txt.py
+++ txt.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+
 window = tk.Tk()
 window.title("Codingal's Text Editor")
 window.geometry("600x500")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
+
 def open_file():
     filepath = askopenfilename(
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
@@ -15,11 +17,11 @@ def open_file():
     with open(filepath, "r") as input_file:
         text = input_file.read()
         txt_edit.insert(tk.END, text)
-        input_file.close()
-    window(f"Codingal's Text Editor - {filepath}")
+    window.title(f"Codingal's Text Editor - {filepath}")
+
 def save_file():
     filepath = asksaveasfilename(
-        defaultextension="txt",
+        defaultextension=".txt",
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
     )
     if not filepath:
@@ -28,12 +30,15 @@ def save_file():
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
     window.title(f"Codingal's Text Editor - {filepath}")
-txt.edit = Text(window)
+
+txt_edit = tk.Text(window)
 fr_btns = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(fr_btns, text="open", command=open_file)
-btn_save = tk.Button(fr_btns, text="Save as....", command=save_file)
+btn_open = tk.Button(fr_btns, text="Open", command=open_file)
+btn_save = tk.Button(fr_btns, text="Save As...", command=save_file)
+
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 fr_btns.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky=nsew)
-window.mainloop()   
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+window.mainloop()
